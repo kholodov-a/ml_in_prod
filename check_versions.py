57acb53461e276e0a530afbeee000c0e9d839256
Alexey Kholodov <84579815+kholodov-a@users.noreply.github.com>
2025-01-15 13:22:13 +0100

    Update check_versions.py

--- check_versions.py
+++ check_versions.py
@@ -1,16 +1,8 @@
 import importlib.metadata
-# import keras
-# import tensorflow as tf
-import flask
-import numpy as np
-import pandas as pd
-from PIL import Image
-# import gunicorn
 
 print("Keras version:", importlib.metadata.version("keras"))
-print("TensorFlow version:", importlib.metadata.version("tensorflow")
+print("TensorFlow version:", importlib.metadata.version("tensorflow"))
 print("Flask version:", importlib.metadata.version("flask"))
-print("NumPy version:", np.__version__)
-print("Pandas version:", pd.__version__)
-print("Pillow version:", Image.__version__)
-# print("Gunicorn version:", gunicorn.__version__)
+print("NumPy version:", importlib.metadata.version("NumPy"))
+print("Pandas version:", importlib.metadata.version("pandas"))
+print("Pillow version:", importlib.metadata.version("Image"))
